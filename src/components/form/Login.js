ae879eb6eed9ed17621a199bbdbb1e166754fe13
Sayem92 <sayemmiha123@gmail.com>
2022-11-07 00:04:34 +0600

    add form

--- src/components/form/Login.js
+++ src/components/form/Login.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import logo from '../../assets/logo.webp'
+import google from '../../assets/Google.png'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <div className='flex justify-center items-center my-5'>
+                <img className='h-20 w-20 ml-5' src={logo} alt="" />
+                <h1 className='text-2xl text-center font-bold text-blue-500'>Helping Team</h1>
+            </div>
+
+            <div className='lg:mx-96 border lg:p-20 rounded-lg bg-slate-100'>
+                <h1 className='text-3xl text-center font-bold text-yellow-400 mb-16'>Login with</h1>
+                <div className='flex justify-between items-center rounded-2xl border border-gray-500 p-2 mb-3'>
+                    <img className='w-8 h-8' src={google} alt="" />
+                    <p className='font-medium'>Continue with Google</p>
+                </div>
+
+                   <p>Don't have an account? <Link to='/register' className='text-blue-500 underline'>Create an account</Link></p>
+            </div>
         </div>
     );
 };
--- src/components/form/Register.js
+++ src/components/form/Register.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import logo from '../../assets/logo.webp'
 
 const Register = () => {
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <div className='flex justify-center items-center my-5'>
+                <img className='h-20 w-20 ml-5' src={logo} alt="" />
+                <h1 className='text-2xl text-center font-bold text-blue-500'>Helping Team</h1>
+            </div>
+
+            <div className='lg:mx-32 border lg:p-20 rounded-lg bg-slate-100'>
+                <h1 className='text-3xl text-center font-bold text-yellow-400 mb-16'>Register as a volunteer</h1>
+                <form>
+                    <div class="relative z-0 mb-6 w-full group">
+                        <input type="text" name="name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+                        <label for="name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Full Name</label>
+                    </div>
+                    <div class="relative z-0 mb-6 w-full group">
+                        <input type="email" name="email" id="email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+                        <label for="email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                    </div>
+                    <div class="relative z-0 mb-6 w-full group">
+                        <input type="password" name="password" id="password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                    </div>
+                    <div class="relative z-0 mb-6 w-full group">
+                        <input type="text" name="date" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+                        <label for="date" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Date</label>
+                    </div>
+                    <div class="grid md:grid-cols-2 md:gap-6">
+                        <div class="relative z-0 mb-6 w-full group">
+                            <input type="text" name="description" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+                            <label for="description" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Description</label>
+                        </div>
+                        <div class="relative z-0 mb-6 w-full group">
+                            <input type="text" name="book" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+                            <label for="book" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Organize book at the library</label>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Registration</button>
+                </form>
+
+            </div>
         </div>
     );
 };
